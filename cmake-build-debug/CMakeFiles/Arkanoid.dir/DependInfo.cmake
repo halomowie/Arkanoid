ad8f33c7f219a1287b3fcef460ed7af2fd3b51b9
halomowie <50609253+halomowie@users.noreply.github.com>
2019-06-07 14:11:13 +0200

    Arkanoid v2

---
+++ cmake-build-debug/CMakeFiles/Arkanoid.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Daniel/Desktop/Cpp/Arkanoid/GameManager.cpp" "C:/Users/Daniel/Desktop/Cpp/Arkanoid/cmake-build-debug/CMakeFiles/Arkanoid.dir/GameManager.cpp.obj"
+  "C:/Users/Daniel/Desktop/Cpp/Arkanoid/LevelDraw.cpp" "C:/Users/Daniel/Desktop/Cpp/Arkanoid/cmake-build-debug/CMakeFiles/Arkanoid.dir/LevelDraw.cpp.obj"
+  "C:/Users/Daniel/Desktop/Cpp/Arkanoid/background.cpp" "C:/Users/Daniel/Desktop/Cpp/Arkanoid/cmake-build-debug/CMakeFiles/Arkanoid.dir/background.cpp.obj"
+  "C:/Users/Daniel/Desktop/Cpp/Arkanoid/ball.cpp" "C:/Users/Daniel/Desktop/Cpp/Arkanoid/cmake-build-debug/CMakeFiles/Arkanoid.dir/ball.cpp.obj"
+  "C:/Users/Daniel/Desktop/Cpp/Arkanoid/main.cpp" "C:/Users/Daniel/Desktop/Cpp/Arkanoid/cmake-build-debug/CMakeFiles/Arkanoid.dir/main.cpp.obj"
+  "C:/Users/Daniel/Desktop/Cpp/Arkanoid/paddle.cpp" "C:/Users/Daniel/Desktop/Cpp/Arkanoid/cmake-build-debug/CMakeFiles/Arkanoid.dir/paddle.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/cpp/SFML-2.5.1/include/SFML"
+  "C:/cpp/SFML-2.5.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Arkanoid.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Arkanoid.dir/main.cpp.obj"
+  "CMakeFiles/Arkanoid.dir/background.cpp.obj"
+  "CMakeFiles/Arkanoid.dir/paddle.cpp.obj"
+  "CMakeFiles/Arkanoid.dir/ball.cpp.obj"
+  "CMakeFiles/Arkanoid.dir/LevelDraw.cpp.obj"
+  "CMakeFiles/Arkanoid.dir/GameManager.cpp.obj"
+  "Arkanoid.pdb"
+  "Arkanoid.exe"
+  "Arkanoid.exe.manifest"
+  "libArkanoid.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Arkanoid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
